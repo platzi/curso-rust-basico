a81d3552ae698c2cf04382aa57ea3798f58d0db3
Hector Pulido <hector.pulido@platzi.com>
2022-03-28 16:52:14 -0500

    Recibiendo nombre y edad

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hola Platzi");
+    println!("Por favor introduce tu nombre: ");
+
+    let mut nombre : String = String::new();
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+
+    // Obtener la edad de la consola
+    println!("Por favor introduce tu edad: ");
+    let mut edad : String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+
+    // Convertir esa edad a un numero
+    let edad_int : u8 = edad.trim().parse().unwrap();
+
+    println!("Hola, bienvenido o bienvenida {} de {} años", nombre, edad_int);
 }
