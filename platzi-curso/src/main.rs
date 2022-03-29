65f434e5dfd9773038513bcf4dd23c64c4070bf3
Hector Pulido <hector.pulido@platzi.com>
2022-03-29 15:55:33 -0500

    funcion sumar uno

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,3 +1,21 @@
+fn sumar_uno(numero_a_sumar: i32) -> i32 {
+    let numero_final = numero_a_sumar + 1;
+    println!("{}", numero_final);
+
+
+    return numero_final;
+}
+
+
+
 fn main() {
     println!("Hola Platzi");
+
+    let diez_mas_uno = sumar_uno(10);
+    sumar_uno(diez_mas_uno);
+    sumar_uno(12);
+    sumar_uno(13);
+    sumar_uno(14);
+
+
 }
