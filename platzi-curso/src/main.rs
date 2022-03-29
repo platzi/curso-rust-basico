329ca7f3c196d866afd288a4809aa9ecfb1caddd
Hector Pulido <hector.pulido@platzi.com>
2022-03-29 16:34:12 -0500

    leer archivos desde archivo de texto

--- platzi-curso/src/main.rs
+++ platzi-curso/src/main.rs
@@ -1,3 +1,21 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str = "history.csv";
+
+
+
 fn main() {
-    println!("Hola Platzi");
+    let content = fs::read_to_string(FILENAME).unwrap();
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+
+        println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+
+
+
+    }
+
+
 }
